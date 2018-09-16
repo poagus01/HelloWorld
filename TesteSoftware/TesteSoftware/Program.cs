8593266bf2a8393262fd7988a2c96f8fb17f9f9e
poagus01 <igustavomichel@gmail.com>
2018-09-16 20:42:34 -0300

    Versao 1.0

---
+++ TesteSoftware/TesteSoftware/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TesteSoftware
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("versão 1.0 oficial");
+            Console.Read();
+        }
+    }
+}
